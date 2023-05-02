1566e22c6dbef70a2f4060b8fb7bc90d1bdfec23
bryanvanmoer <bryan.vanmoer51@hotmail.com>
2023-05-02 12:26:08 +0200

    controller & labo files

---
+++ labo.c
@@ -0,0 +1,16 @@
+#include <string.h>
+#include <libgen.h>
+#include <fcntl.h>
+#include <ctype.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include <signal.h>
+
+
+int main(int argc, char** argv){
+
+	
+}
+
+
+
